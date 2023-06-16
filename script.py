6a8d4e60551bd69eee6291e177a7ef2f4f47a76b
Avneet Singh <avneetsingh@avneets-mbp.lan>
2023-06-16 04:16:27 -0400

    add more protectino

--- script.py
+++ script.py
@@ -1,17 +1,21 @@
 # This script will take control of your computer (to some extent) to help you take steps to level up your Pokemon in DayCare --> Check out README.md for more details!
 
+# Import necessary libraries
 import pyautogui
 import keyboard
 from time import sleep
 
+x = 10
+y = 10
+
+# Log and Print screen dimentions so that you know exactly which monitor is being used (this can be removed)
 screenWidth, screenHeight = pyautogui.size()
 print(screenWidth, screenHeight)
+sleep(2)
 
-sleep(3)
-
-# Uncomment if you wanna test that it clicks a certain portion of your screen
-#pyautogui.click(160,60)
-#sleep(1)
+# Uncomment if you want test that it clicks a certain portion of your screen; make sure to set the x and y position above
+pyautogui.click(x,y)
+sleep(2)
 
 for i in range(10):
     c1 = 50
@@ -22,3 +26,5 @@ for i in range(10):
     while c2 > 0:
         pyautogui.press('down')
         c2-=1
+        
+print('Done')
