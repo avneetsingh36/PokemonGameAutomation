e2993e642aa1532aac2b8bedb7ccd78721940d0a
Avneet Singh <avneetsingh@avneets-mbp.lan>
2023-06-16 04:33:14 -0400

    refactor

--- script.py
+++ script.py
@@ -1,30 +1,46 @@
-# This script will take control of your computer (to some extent) to help you take steps to level up your Pokemon in DayCare --> Check out README.md for more details!
+# This script will take control of your computer (to some extent) 
+# to help you take steps to level up your Pokemon in DayCare 
+# --> Check out README.md for more details!
 
 # Import necessary libraries
 import pyautogui
-import keyboard
 from time import sleep
 
-x = 10
-y = 10
+def get_screen_size():
+    """Log and print screen dimensions"""
+    screen_width, screen_height = pyautogui.size()
+    print(screen_width, screen_height)
+    sleep(2)
+    return screen_width, screen_height
 
-# Log and Print screen dimentions so that you know exactly which monitor is being used (this can be removed)
-screenWidth, screenHeight = pyautogui.size()
-print(screenWidth, screenHeight)
-sleep(2)
+def click_position(x, y):
+    """Click a specific position on screen"""
+    pyautogui.click(x, y)
+    sleep(2)
 
-# Uncomment if you want test that it clicks a certain portion of your screen; make sure to set the x and y position above
-pyautogui.click(x,y)
-sleep(2)
+def press_keys():
+    """Simulate key presses"""
+    for i in range(10):
+        c1 = 50
+        c2 = 50
+        while c1 > 0:
+            pyautogui.press('up')
+            c1 -= 1
+        while c2 > 0:
+            pyautogui.press('down')
+            c2 -= 1
 
-for i in range(10):
-    c1 = 50
-    c2 = 50
-    while c1 > 0:
-        pyautogui.press('up')
-        c1-=1
-    while c2 > 0:
-        pyautogui.press('down')
-        c2-=1
-        
-print('Done')
+def main():
+    get_screen_size()
+
+    # Uncomment the following line and set x and y to test if it clicks a certain portion of your screen.
+    # This allows you to have the necessary tab clicked as a part of the automation process.
+    # click_position(x, y)
+    
+    press_keys()
+
+    print('Done')
+
+if __name__ == "__main__":
+    main()
+
